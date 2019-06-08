0535650fcd7623d1ce6e3daf7675d2d5739ea2fd
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-08 16:26:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     params[:team][:members].each do |name| 
       i+=1
       name.each do |x,value|
-        instance_variable_set("@#{x}_#{i}", "Hero #{x.capitalize}: "value) 
+        instance_variable_set("@#{x}_#{i}", "Hero #{x.capitalize}: " + value) 
       end 
     end
       erb :team 
